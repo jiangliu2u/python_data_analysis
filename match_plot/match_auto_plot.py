95b190e275233b5f47a7182da40ac02644b31030
jiangliu2u <jiangliu2u@gmail.com>
2018-04-24 15:10:39 +0800

    a

--- match_plot/match_auto_plot.py
+++ match_plot/match_auto_plot.py
@@ -24,27 +24,30 @@ ax1.spines['left'].set_position(('data', 0))#设置y轴的到数据为0的位置
 ax1.set_title("{} vs {} 经济、经验差".format(data['radiant_team']['tag'],data['dire_team']['tag']))#设置标题
 ax1.legend(handles=[gold,xp],labels=["经济","经验"],loc='upper left')#设置图例
 
-#拉野情况
-laye = {i['name']:i['camps_stacked'] for i in data['players']}
+#输出
+laye = {i['name']:round(i['hero_damage']/i['total_gold'],2) for i in data['players']}
 la = pd.Series(laye)
 ax2 = fig.add_subplot(222)
 a=la.sort_values(ascending=True)
-b=a[a.values>0]
-names = [i for i in b.index]
+names = [i for i in a.index]
 haha =pd.Series(names)
 ax2.spines['top'].set_color('none')#去除上边框线
 ax2.spines['right'].set_color('none')#去除右边框
 ax2.set_yticklabels(haha.values)
 ax2.set_yticks(haha.index)
-ax2.set_title("堆叠野怪次数")
-ax2.barh(range(len(haha.index)),b.values,color='#c1f585')#水平条形图
-
+ax2.set_title("输出经济比")
+rect = ax2.barh(range(len(haha.index)),a.values,color='#ffa165')#水平条形图
+for rec in rect:#条形图上显示数据标签
+    y=rec.get_y()
+    width=rec.get_width()
+    ax2.text(1.02*width,y+0.2,str(width))
+	
+	
 #选手打出的伤害
 ds = {i['name']:i['hero_damage'] for i in data['players']}
 damages = pd.Series(ds)
 ax3 = fig.add_subplot(223)
 damages=damages.sort_values(ascending=True)
-print(type(damages))
 names=pd.Series([i for i in damages.index])
 ax3.spines['top'].set_color('none')#去除上边框线
 ax3.spines['right'].set_color('none')#去除右边框
@@ -67,6 +70,9 @@ for i in range(10):
 
 ax4.set_title("{} vs {} 选手金钱变化".format(data['radiant_team']['tag'],data['dire_team']['tag']))#设置标题
 ax4.legend(handles=lines,labels=list(gold_t.columns),loc='upper left')#设置图例
+
+
+fig.suptitle("{} vs {} 比赛部分数据对比".format(data['radiant_team']['tag'],data['dire_team']['tag']))
 fig.savefig('{}.png'.format(match_id))
 #plt.show()
 
