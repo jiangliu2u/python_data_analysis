5b32b49080d5ba920c72b2621a7cbe02955b8bef
jiangliu2u <jiangliu2u@gmail.com>
2018-08-08 10:15:53 +0800

    image classify

--- data analysis and mining/chapter6/plot.py
+++ data analysis and mining/chapter6/plot.py
@@ -7,7 +7,7 @@ def plot_cm(y, yp):
     cm = confusion_matrix(y, yp)  # 混淆矩阵
 
     plt.matshow(cm, cmap=plt.cm.Greens)  # 画混淆矩阵图，配色风格使用cm.Greens，更多风格请参考官网。
-    plt.colorbar()  # 颜色标签
+    #plt.colorbar()  # 颜色标签
 
     for x in range(len(cm)):  # 数据标签
         for y in range(len(cm)):
