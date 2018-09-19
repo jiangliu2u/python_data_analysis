8606c9bdbde7083a5ade4e00f746633933f179d7
jiangliu2u <jiangliu2u@gmail.com>
2018-09-19 12:47:48 +0800

    opt

--- excel_processing/从原始数据筛选hs.py
+++ excel_processing/从原始数据筛选hs.py
@@ -5,24 +5,12 @@ input_file = "aa.xls"
 output_file = "hs.xls"
 
 data = pd.read_excel(input_file,header=[1])
-
 companies = [2119,2144, 2552, 2543, 2620, 2549, 2617, 2137, 2590, 2783, 2731, 2580,
              2592, 2561, 2551, 2738, 2550, 2611, 2587, 2618, 2596, 2626, 2581, 2644]  # 分公司代码
-
-index_com = []
-for i  in companies:
-    d = data[data['工厂'] == i].index.tolist()
-    for j in d:
-        index_com.append(j)
-
-index_com = list(set(index_com))
-ff = []
-for i in index_com:
-    ff.append(data.iloc[i, :])  # 根据索引选择行，保存在ff中，再转化为DataFrame导出为excel
-outdata1 = pd.DataFrame(ff)
+outdata1 = data[ data["工厂"].isin(companies)]
 outdata1 = outdata1.reset_index(drop=True)
 outdata1.to_excel(output_file)
-end = datetime.datetime.now()
 print("筛选hs数据完成，文件名为 {}".format(output_file))
+end = datetime.datetime.now()
 print("运行时间 {}".format(end - start))
 
--- excel_processing/筛选_opt.py
+++ excel_processing/筛选_opt.py
@@ -6,7 +6,6 @@ output_file = "final2.xls" #输出文件名称
 
 #读取hs所有信息
 hs_data = pd.read_excel(input_file)
-
 data = pd.concat([hs_data["工厂描述"],hs_data["产品名称"],hs_data["材料名称"],hs_data["理论消耗量"],hs_data["实际消耗量"]],axis=1)
 data1 = data[data["材料名称"].str.contains("盖")]
 data2 = data1[~data1["材料名称"].str.contains("待切|瓶盖料|内垫料")]
@@ -14,8 +13,6 @@ data3 = data2[~data2["产品名称"].str.contains("空罐|底盖")]
 data3 = data3.reset_index(drop=True)
 data3.index.name = "序号"
 data3.to_excel(output_file)
-
-
 print("进一步筛选数据完成，文件名为 {}".format(output_file))
 end = datetime.datetime.now()
 print("运行时间 {}".format(end - start))
